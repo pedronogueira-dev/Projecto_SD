ec460499b269cc45c8ef41dba99d884a587de780
Pedro Nogueira <17pedro.n@gmail.com>
2016-04-05 10:58:45 +0100

    ADDED broker-ws initial files

--- broker-ws/src/main/java/pt/upa/broker/BrokerApplication.java
+++ broker-ws/src/main/java/pt/upa/broker/BrokerApplication.java
@@ -1,9 +1,69 @@
 package pt.upa.broker;
 
+import javax.xml.ws.Endpoint;
+import pt.upa.broker.ws.BrokerPort;
+
+import pt.ulisboa.tecnico.sdis.ws.uddi.UDDINaming;
 public class BrokerApplication {
 
 	public static void main(String[] args) throws Exception {
 		System.out.println(BrokerApplication.class.getSimpleName() + " starting...");
+
+		// Check arguments
+		if (args.length < 3) {
+			System.err.println("Argument(s) missing!");
+			System.err.printf("Usage: java %s uddiURL wsName wsURL%n",	BrokerApplication.class.getName());
+			return;
+		}
+
+		String uddiURL = args[0];
+		String name = args[1];
+		String url = args[2];
+
+		Endpoint endpoint = null;
+		UDDINaming uddiNaming = null;
+		try {
+			BrokerPort port = new	BrokerPort();
+			endpoint = Endpoint.create(port);
+
+			// publish endpoint
+			System.out.printf("Starting %s%n", url);
+			endpoint.publish(url);
+
+			// publish to UDDI
+			System.out.printf("Publishing '%s' to UDDI at %s%n", name, uddiURL);
+			uddiNaming = new UDDINaming(uddiURL);
+			uddiNaming.rebind(name, url);
+
+			// wait
+			System.out.println("Awaiting connections");
+			System.out.println("Press enter to shutdown");
+			System.in.read();
+
+		} catch (Exception e) {
+			System.out.printf("Caught exception: %s%n", e);
+			e.printStackTrace();
+
+		} finally {
+			try {
+				if (endpoint != null) {
+					// stop endpoint
+					endpoint.stop();
+					System.out.printf("Stopped %s%n", url);
+				}
+			} catch (Exception e) {
+				System.out.printf("Caught exception when sto	Broker: %s%n", e);
+			}
+			try {
+				if (uddiNaming != null) {
+					// delete from UDDI
+					uddiNaming.unbind(name);
+					System.out.printf("Deleted '%s' from UDDI%n", name);
+				}
+			} catch (Exception e) {
+				System.out.printf("Caught exception when deleting: %s%n", e);
+			}
+		}
 
 	}
 
--- broker-ws/src/main/java/pt/upa/broker/ws/BrokerPort.java
+++ broker-ws/src/main/java/pt/upa/broker/ws/BrokerPort.java
@@ -1,7 +1,68 @@
 package pt.upa.broker.ws;
 
-public class BrokerPort {
+import java.util.List;
+import java.util.Arrays;
+import javax.jws.WebService;
 
-	// TODO
+@WebService(
+    endpointInterface="pt.upa.broker.ws.BrokerPortType",
+    wsdlLocation="broker.1_0.wsdl",
+    name="BrokerWebService",
+    portName="BrokerPort",
+    targetNamespace="http://ws.broker.upa.pt/",
+    serviceName="BrokerService"
+)
+public class BrokerPort implements BrokerPortType{
+	//private List<String> _NorthRegion;
+	//private List<String> _CenterRegion;
+	//private List<String> _SouthRegion;
 
+	//private List<TransportView> _transportList= new List<TransportView>();
+	
+	//_NorthRegion=Arrays.asList("Porto", "Braga", "Viana do Castelo", "Vila Real", "Braganca");
+	//_CenterRegion=Arrays.asList("Lisboa", "Leiria", "Santarem", "Castelo Branco", "Coimbra", "Aveiro", "Viseu", "Guarda");
+	//_SouthRegion=Arrays.asList("Setúbal","Évora", "Portalegre", "Beja", "Faro");
+
+	public String ping(String name){
+		return null;
+	}
+
+	public String requestTransport(String origin, String destination, int price) throws UnknownLocationFault_Exception,
+	InvalidPriceFault_Exception, UnavailableTransportFault_Exception, UnavailableTransportPriceFault_Exception
+	{	/*boolean isNorthern=_NorthRegion.contains(origin);
+		boolean isSouthern=_CenterRegion.contains(origin);
+		boolean isCentral=_SouthRegion.contains(origin);
+
+		if(!isNorthern||!isCentral||!isSouthern){
+			throw UnknownLocationFault_Exception(origin);
+		}
+
+		boolean isNorthern=_NorthRegion.contains(destination);
+		boolean isSouthern=_CenterRegion.contains(destination);
+		boolean isCentral=_SouthRegion.contains(destination);
+
+		if(!isNorthern||!isCentral||!isSouthern){
+			throw UnknownLocationFault_Exception(destination);
+		}
+
+		if(price <0){
+			throw new InvalidPriceFault_Exception(price);
+		}
+		*/
+		//HOW TO CHECK THE LIST?
+		return new String("requestTransport invocation accepted");
+	}
+
+	public TransportView viewTransport(String id) throws UnknownTransportFault_Exception
+	{
+		return null;
+	}
+
+	public List<TransportView> listTransports(){
+		return null;
+	}
+
+	public void clearTransports(){
+		return;
+	}
 }
