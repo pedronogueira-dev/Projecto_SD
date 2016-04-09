78bff5bacecf5e7f61d11dcb6df3832ec580c409
Pedro Nogueira <17pedro.n@gmail.com>
2016-04-09 20:46:53 +0100

    Transporter Connecting with ping working

--- transporter-ws-cli/src/main/java/pt/upa/transporter/TransporterClientApplication.java
+++ transporter-ws-cli/src/main/java/pt/upa/transporter/TransporterClientApplication.java
@@ -1,9 +1,59 @@
 package pt.upa.transporter;
 
+import pt.upa.transporter.ws.TransporterService;
+import pt.upa.transporter.ws.TransporterPortType;
+
+import pt.upa.transporter.ws.cli.TransporterClient;
+
+
+import static javax.xml.ws.BindingProvider.ENDPOINT_ADDRESS_PROPERTY;
+import java.util.Map;
+import javax.xml.ws.BindingProvider;
+import pt.ulisboa.tecnico.sdis.ws.uddi.UDDINaming;
+
 public class TransporterClientApplication {
 
 	public static void main(String[] args) throws Exception {
 		System.out.println(TransporterClientApplication.class.getSimpleName() + " starting...");
 
+
+// Check arguments
+		if (args.length < 2) {
+			System.err.println("Argument(s) missing!");
+			System.err.printf("Usage: java %s uddiURL name%n", TransporterClient.class.getName());
+			return;
+		}
+
+		String uddiURL = args[0];
+		String name = args[1];
+
+		System.out.printf("Contacting UDDI at %s%n", uddiURL);
+		UDDINaming uddiNaming = new UDDINaming(uddiURL);
+
+		System.out.printf("Looking for '%s'%n", name);
+		String endpointAddress = uddiNaming.lookup(name);
+
+		if (endpointAddress == null) {
+			System.out.println("Not found!");
+			return;
+		} else {
+			System.out.printf("Found %s%n", endpointAddress);
+		}
+
+		System.out.println("Creating stub ...");
+		TransporterService service = new TransporterService();
+		TransporterPortType port = service.getTransporterPort();
+
+		System.out.println("Setting endpoint address ...");
+		BindingProvider bindingProvider = (BindingProvider) port;
+		Map<String, Object> requestContext = bindingProvider.getRequestContext();
+		requestContext.put(ENDPOINT_ADDRESS_PROPERTY, endpointAddress);
+
+		try {
+			TransporterClient client=new TransporterClient(port);
+			System.out.println(client.ping("Client1"));
+		} catch (Exception pfe) {
+			System.out.println("Caught: " + pfe);
+		}
 	}
 }
--- transporter-ws-cli/src/main/java/pt/upa/transporter/ws/cli/TransporterClient.java
+++ transporter-ws-cli/src/main/java/pt/upa/transporter/ws/cli/TransporterClient.java
@@ -1,7 +1,43 @@
 package pt.upa.transporter.ws.cli;
 
+
+import pt.upa.transporter.ws.TransporterPortType;
+import pt.upa.transporter.ws.BadLocationFault_Exception;
+import pt.upa.transporter.ws.BadPriceFault_Exception;
+import pt.upa.transporter.ws.BadJobFault_Exception;
+import pt.upa.transporter.ws.JobView;
+
+import java.util.List;
+
 public class TransporterClient {
 
-	// TODO
+	private TransporterPortType _tc;
+	public TransporterClient(TransporterPortType tc){
+			_tc=tc;
+	}
+	public String ping(String name){
+		return _tc.ping(name);
+	}
+
+
+    public JobView requestJob(String origin,String destination,int price) throws BadLocationFault_Exception, BadPriceFault_Exception{
+    	return null;
+    }
+
+    public JobView decideJob(String id,boolean accept) throws BadJobFault_Exception{
+    	return null;
+    }
+
+   	public JobView jobStatus(String id){
+   		return null;
+   	}
+
+    public List<JobView> listJobs(){
+    	return null;
+    }
+
+    public void clearJobs(){
+
+    }
 
 }
