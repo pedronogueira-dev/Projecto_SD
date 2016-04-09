c8b0037e31cf082dbfe50868c492b375d840234b
Pedro Nogueira <17pedro.n@gmail.com>
2016-04-09 19:57:34 +0100

    .

--- transporter-ws/src/main/java/pt/upa/transporter/TransporterApplication.java
+++ transporter-ws/src/main/java/pt/upa/transporter/TransporterApplication.java
@@ -1,10 +1,70 @@
 package pt.upa.transporter;
 
+
+import javax.xml.ws.Endpoint;
+import pt.upa.transporter.ws.TransporterPort;
+
+import pt.ulisboa.tecnico.sdis.ws.uddi.UDDINaming;
 public class TransporterApplication {
 
 	public static void main(String[] args) throws Exception {
 		System.out.println(TransporterApplication.class.getSimpleName() + " starting...");
 
+		// Check arguments
+		if (args.length < 3) {
+			System.err.println("Argument(s) missing!");
+			System.err.printf("Usage: java %s uddiURL wsName wsURL%n",	TransporterApplication.class.getName());
+			return;
+		}
+
+		String uddiURL = args[0];
+		String name = args[1];
+		String url = args[2];
+
+		Endpoint endpoint = null;
+		UDDINaming uddiNaming = null;
+		try {
+			TransporterPort port = new	TransporterPort();
+			endpoint = Endpoint.create(port);
+
+			// publish endpoint
+			System.out.printf("Starting %s%n", url);
+			endpoint.publish(url);
+
+			// publish to UDDI
+			System.out.printf("Publishing '%s' to UDDI at %s%n", name, uddiURL);
+			uddiNaming = new UDDINaming(uddiURL);
+			uddiNaming.rebind(name, url);
+
+			// wait
+			System.out.println("Awaiting connections");
+			System.out.println("Press enter to shutdown");
+			System.in.read();
+
+		} catch (Exception e) {
+			System.out.printf("Caught exception: %s%n", e);
+			e.printStackTrace();
+
+		} finally {
+			try {
+				if (endpoint != null) {
+					// stop endpoint
+					endpoint.stop();
+					System.out.printf("Stopped %s%n", url);
+				}
+			} catch (Exception e) {
+				System.out.printf("Caught exception when sto	Broker: %s%n", e);
+			}
+			try {
+				if (uddiNaming != null) {
+					// delete from UDDI
+					uddiNaming.unbind(name);
+					System.out.printf("Deleted '%s' from UDDI%n", name);
+				}
+			} catch (Exception e) {
+				System.out.printf("Caught exception when deleting: %s%n", e);
+			}
+		}
 	}
 
 }
--- transporter-ws/src/main/java/pt/upa/transporter/ws/TransporterPort.java
+++ transporter-ws/src/main/java/pt/upa/transporter/ws/TransporterPort.java
@@ -1,7 +1,37 @@
 package pt.upa.transporter.ws;
 
-public class TransporterPort {
+import java.util.List;
+import javax.jws.WebService;
+@WebService(
+    endpointInterface="pt.upa.transporter.ws.TransporterPortType",
+    wsdlLocation="transporter.1_0.wsdl",
+    name="TransporterWebService",
+    portName="TransporterPort",
+    targetNamespace="http://ws.transporter.upa.pt/",
+    serviceName="TransporterService"
+)
+public class TransporterPort implements TransporterPortType {
 
-	// TODO
+	public String ping(String name){
+		return "Transporter: "+ name+ " Connected to TransporterServer";
+	}
 
+	public JobView requestJob(String origin,String destination,int price)throws BadLocationFault_Exception, BadPriceFault_Exception{
+		return null;
+	}
+
+	public JobView decideJob(String id,boolean accept)throws BadJobFault_Exception{
+		return null;
+    }
+
+    public JobView jobStatus(String id){
+    	return null;
+    }
+
+    public List<JobView> listJobs(){
+    	return null;
+    }
+
+    public void clearJobs(){
+    }
 }
